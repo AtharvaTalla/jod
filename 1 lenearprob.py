1fc4fb3ac2b55d7b452e317530dd4d079bb89a22
AtharvaTalla <148702004+AtharvaTalla@users.noreply.github.com>
2024-05-08 01:32:58 +0530

    Update 1 lenearprob.py

--- 1 lenearprob.py
+++ 1 lenearprob.py
@@ -1,12 +1,11 @@
 import array as hashTable
-#Create an empty array of size 7 and Initialize all index to -1
 size=7
 hashTable = hashTable.array('i',[-1]*size)
 
 def initial():
-    print(hashTable) # Print empty hash table.
+    print(hashTable) 
 
-def insertHash(value): #this function is used to insert values into hash table
+def insertHash(value): 
     key = value % size
     if(hashTable[key] == -1):
         hashTable[key] = value
@@ -20,28 +19,28 @@ def insertHash(value): #this function is used to insert values into hash table
             if(hashTable[i]!=-1):
                 count+=1
             i+=1
-        if(count==size):#checking for the hash full
+        if(count==size):
             print("Hash Table Is Full Hence ",value," Can not Be Inserted")
             print(hashTable)
-        else:  #logic of linear probiing
+        else: 
             i=key
             for i in range(key, size):
-                key1=(i+1)%size  #calculating next empty location address using linear probing formula
-                if(hashTable[key1] == -1): #if location is empty insert data 
+                key1=(i+1)%size  
+                if(hashTable[key1] == -1): 
                     hashTable[key1] = value
                     print(value,"inserted at arr",key1)
                     break
         
         
 
-def search(value): #this function is used to search value into hash table
+def search(value): 
     key = value % size
     if(hashTable[key]==value):
         print(value, "Found at", key,"th Index location")
     else:
         print(value, "Not Found at", key,"th Index location")
         
-def delHash(value): #this function is used to delete value from hash table if present
+def delHash(value):
     key = value % size
     if(hashTable[key]==value):
         print(value, "Sussfully deleted from hash table")
@@ -49,7 +48,7 @@ def delHash(value): #this function is used to delete value from hash table if pr
     else:
         print(value, "Not Found at", key,"th Index location") 
 
-initial() #Actual code starting to execute from this line.            
+initial()             
 insertHash(7) 
 insertHash(10)
 insertHash(40)
@@ -59,11 +58,53 @@ insertHash(28)
 insertHash(32)
 insertHash(5)
 
-print(hashTable) # Print hash table along with data.
+print(hashTable) 
 
-search(13) #searching operation in hash table
+search(13)
 search(25)
 
-delHash(13) #delete operation in hash table
+delHash(13) 
 for i in range(0,7):
-    print("Index[",i,"]=", hashTable[i]) # Print hash table along with data.
+    print("Index[",i,"]=", hashTable[i]) 
+
+
+
+
+
+
+
+
+
+
+
+# ALGORITHM:
+# Function linear_probe(key, value):
+# i = key
+# for i in range(key, size):
+# key1 = (i + 1) % size # Calculating next empty location address using linear probing formula
+# if hashTable[key1] == -1: # If location is empty, insert data
+# hashTable[key1] = value
+# print(value, &quot;inserted at arr&quot;, key1)
+# break
+
+# Function quadratic_probe(key, value):
+# for i in range(key, size):
+# key1 = (key + i*i) % size # Quadratic Probing formula
+# if hashTable[key1] == -1:
+# hashTable[key1] = value
+# print(value, &quot;inserted at arr&quot;, key1)
+# break
+
+# Main Function :
+# size = 7
+# hashTable = array(&#39;i&#39;, [-1] * size)
+
+# # Example usage of linear probing
+# key = &lt;some_key&gt;
+# value = &lt;some_value&gt;
+# linear_probe(key, value)
+
+# # Example usage of quadratic probing
+# key = &lt;some_key&gt;
+# value = &lt;some_value&gt;
+# quadratic_probe(key, value)
