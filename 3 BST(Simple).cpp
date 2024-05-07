b01394d9426d0ce80d655f32037dfc9cf1056b28
AtharvaTalla <148702004+AtharvaTalla@users.noreply.github.com>
2024-05-08 01:34:44 +0530

    Update 3 BST(Simple).cpp

--- 3 BST(Simple).cpp
+++ 3 BST(Simple).cpp
@@ -152,6 +152,119 @@ int main() {
 
 
 
+// ALGORITHM :
+// Algorithm CreateBinaryTree():
+// // This algorithm is used to create a binary tree with no input and return nothing.
+
+// Repeat
+// Read data element from the user into &#39;Value&#39;;
+// Create a new node;
+// temp = new node;
+// temp-&gt;data = value;
+// temp-&gt;left = NULL;
+// temp-&gt;right = NULL;
+// If (root is NULL) then // Tree is Empty.
+// root = temp;
+// Else // Add a new node into the existing tree.
+// Insert(root, temp);
+// Until (false);
+
+// Algorithm Insert(root, temp):
+// // This algorithm is used to add a new node in an existing tree pointed to by root. It returns
+// nothing.
+// If (root is not NULL)
+// {
+// If (root-&gt;data &gt; temp-&gt;data) then // Left Subtree
+// {
+// If (temp-&gt;left is NULL) then // Left child is NULL
+// temp-&gt;left = temp;
+// Else
+// Insert(temp-&gt;left, temp); // Check next left link
+// }
+// Else
+// {
+// If (ch is Right side) then // Right Subtree
+// {
+//     If (temp-&gt;right is NULL) then // Right child is NULL
+// temp-&gt;right = temp;
+// Else
+// Insert(temp-&gt;right, temp); // Check next right link
+// }
+// }
+// }
+// Algorithm Preorder(temp):
+// // This algorithm is used to display the nodes in VLR (Value-Left-Right) way.
+// If (temp is not NULL) then
+// {
+// Write(temp-&gt;data);
+// Call Preorder(temp-&gt;lptr);
+// Call Preorder(temp-&gt;rptr);
+// }
+// Algorithm Inorder(temp):
+// // This algorithm is used to display the nodes in LVR (Left-Value-Right) way.
+// If (temp is not NULL) then
+// {
+// Call Inorder(temp-&gt;lptr);
+// Write(temp-&gt;data);
+// Call Inorder(temp-&gt;rptr);
+// }
+// Algorithm Postorder(temp):
+// // This algorithm is used to display the nodes in LRV (Left-Right-Value) way.
+// If (temp is not NULL) then
+// {
+// Call Postorder(temp-&gt;lptr);
+// Call Postorder(temp-&gt;rptr);
+// Write(temp-&gt;data);
+// }
+// Algorithm Search(root, val, parent):
+// // This algorithm is used to search for an element in a Binary Search Tree (BST).
+// If (root is not NULL) then // Tree exists.
+// {
+// If (root-&gt;data &gt; val) then // Search in the Left subtree.
+// Search(root-&gt;llink, val, root);
+// Else If (root-&gt;data &lt; val) then // Search in the Right subtree.
+// Search(root-&gt;rlink, val, root);
+// If (root-&gt;data == val) then // Successful search.
+// {
+// Write(&quot;Value is found successfully&quot;);
+// Return parent;
+// }
+// }
+// Else // Unsuccessful search.
+// Return NULL;
+
+// Algorithm Smallest(root):
+// // This algorithm finds the minimum data value found in the tree.
+// While (root-&gt;ln is not NULL)
+// {
+// root = root-&gt;ln;
+// }
+// Print root-&gt;key;
+
+// Algorithm LongestPath(root):
+// // This algorithm finds the number of nodes in the longest path in the tree.
+// If (root is NULL)
+// Return 0;
+// L = LongestPath(root-&gt;ln);
+// R = LongestPath(root-&gt;rn);
+// If (L &gt; R)
+// Return (L + 1);
+// else
+// Return (R + 1);
+
+// Algorithm swapNodes(root):
+// // This algorithm changes a tree so that the roles of the left and right pointers are swapped at every
+// node.
+// Node* temp;
+// If (root is NULL)
+// Return NULL;
+// temp = root-&gt;ln;
+// root-&gt;ln = root-&gt;rn;
+// root-&gt;rn = temp;
+// swapNodes(root-&gt;ln);
+// swapNodes(root-&gt;rn);
+
+
 
 
 
