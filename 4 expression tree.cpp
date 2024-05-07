fa7ae63b1fab8556a29ac9bfeaa1ac5649b2b43c
AtharvaTalla <148702004+AtharvaTalla@users.noreply.github.com>
2024-05-07 21:49:56 +0530

    Update 4 expression tree.cpp

--- 4 expression tree.cpp
+++ 4 expression tree.cpp
@@ -1,33 +1,23 @@
 #include <iostream>
 #include <stack>
 #include <cctype>
-
 using namespace std;
-
-// Expression Tree Node
 struct TreeNode {
     char data;
     TreeNode* left;
     TreeNode* right;
-    
     TreeNode(char d) : data(d), left(nullptr), right(nullptr) {}
 };
-
-// Function to check if character is an operator
 bool isOperator(char c) {
     return c == '+' || c == '-' || c == '*' || c == '/';
 }
-
-// Function to construct expression tree from prefix expression
 TreeNode* constructExpressionTree(string prefixExpr) {
     stack<TreeNode*> stk;
-    
-    // Traverse the prefix expression in reverse order
     for (int i = prefixExpr.length() - 1; i >= 0; --i) {
         char c = prefixExpr[i];
-        if (isalnum(c)) { // If operand, create a node and push onto stack
+        if (isalnum(c)) { 
             stk.push(new TreeNode(c));
-        } else if (isOperator(c)) { // If operator, create a node and attach left and right children
+        } else if (isOperator(c)) { 
             TreeNode* newNode = new TreeNode(c);
             newNode->left = stk.top();
             stk.pop();
@@ -37,15 +27,12 @@ TreeNode* constructExpressionTree(string prefixExpr) {
         }
     }
     
-    // Root of the expression tree will be at the top of stack
+   
     return stk.top();
 }
-
-// Function to traverse expression tree using post-order traversal (non-recursive)
 void postOrderTraversal(TreeNode* root) {
     if (!root)
         return;
-
     stack<TreeNode*> stk;
     TreeNode* current = root;
     TreeNode* lastVisited = nullptr;
@@ -67,7 +54,6 @@ void postOrderTraversal(TreeNode* root) {
     }
 }
 
-// Function to delete the entire expression tree
 void deleteTree(TreeNode* root) {
     if (root) {
         deleteTree(root->left);
@@ -79,13 +65,9 @@ void deleteTree(TreeNode* root) {
 int main() {
     string prefixExpr = "+--a*bc/def";
     TreeNode* root = constructExpressionTree(prefixExpr);
-
     cout << "Post-order Traversal: ";
     postOrderTraversal(root);
     cout << endl;
-
-    // Deleting the entire tree to avoid memory leaks
     deleteTree(root);
-
     return 0;
 }
